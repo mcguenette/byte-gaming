9207bfb1f6d0fc0bf84ae3c86fe6f53505695b8a
MaksimDimov <maksim.dimov228@gmail.com>
2024-06-17 20:29:23 +0300

    Maksim first commit Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import HeroContact from '../components/HeroContact';
+import Header from '../components/Header';
+import ContactContact from '../components/ContactContact';
+
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+    <HeroContact />
+    <div className='container'>
+     <ContactContact />S
+    </div>
+    </div>
   )
 }
 
