37524e24332acb45d272a1a35f31cdab19051c2a
MaksimDimov <maksim.dimov228@gmail.com>
2024-06-20 19:41:53 +0300

    final toches with email sending

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -6,9 +6,13 @@ function ContactForm({ contactFormRef }) {
     const [ContactName, setName] = useState('');
     const [ContactEmail, setEmail] = useState('');
     const [ContactMessage, setMessage] = useState('');
+    const [emailMessage, setEmailMessage] = useState('');
+    const [formClass, setFormClass] = useState('big-margin-bottom');
 
     const sendEmail = async (e) => {
         e.preventDefault(); // Prevent form submission from reloading the page
+
+        setFormClass('small-margin-bottom'); // Update the form class
 
         try {
             const response = await fetch('https://bytegamingapi.azurewebsites.net/contact-request/add', {
@@ -30,15 +34,17 @@ function ContactForm({ contactFormRef }) {
             const data = await response.json();
             console.log('Response data:', data);
             console.log('all good');
+            setEmailMessage("The email was sent");
         } catch (error) {
-            console.error( error);
+            console.error("Error:", error);
+            setEmailMessage("Error sending email");
         }
     };
 
     return (
         <div className="contact-form" ref={contactFormRef}>
             <h2>Please feel free to text us</h2>
-            <form className="contact-form-form" onSubmit={sendEmail}>
+            <form className={`contact-form-form ${formClass}`} onSubmit={sendEmail}>
                 <label className="contact-form-label">
                     <p>Name:</p>
                     <input type="text" value={ContactName} onChange={(e) => setName(e.target.value)} />
@@ -52,9 +58,11 @@ function ContactForm({ contactFormRef }) {
                     <textarea value={ContactMessage} onChange={(e) => setMessage(e.target.value)} />
                 </label>
                 <Button text="Submit" className={"primary submit-button"} />
+                <p className="email-acception">{emailMessage}</p>
             </form>
         </div>
     );
 }
 
 export default ContactForm;
+
