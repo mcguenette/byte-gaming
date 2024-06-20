5b3f5a3ef0b98885e880f3184a62f81249f00d8b
MaksimDimov <maksim.dimov228@gmail.com>
2024-06-20 06:17:17 +0300

    final fixes

--- src/components/contact/GetInTouch.jsx
+++ src/components/contact/GetInTouch.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable react/prop-types */
 
 import Button from '../Button';
@@ -32,43 +31,9 @@ function GetInTouch({ contactFormRef }) {
                     <Button className='contact-inside-option' text="Chat with us" />
                 </div>
             </div>
-=======
-import React from 'react';
-
-function GetInTouch() {
-    return (
-        <div>
-        <h2 className='contact-first-words'><b>Feel free to contact us</b></h2>
-        <p>
-          We'd love to hear from you! Whether you have questions, 
-          feedback, or need assistance, our team is here to help. 
-          Reach out to us through any of the methods below, and we’ll 
-          get back to you as soon as possible.
-        </p>
-        <div className='contact-options'>
-          <div className='contact-option'>
-            <h2 className='contact-title contact-inside-option'>Phone</h2>
-            <p className='contact-inside-option'>Call us directly</p>
-            <p className='contact-inside-option'>(123) 456-7890</p>
-          </div>
-          <div className='contact-option'>
-            <h2 className='contact-title contact-inside-option'>Email</h2>
-            <p className='contact-inside-option'>Send us an email directly</p>
-            <button className='contact-inside-option'>Email us</button>
-        </div>
-          <div className='contact-option'>
-            <h2 className='contact-title contact-inside-option'>Chat</h2>
-            <button className='contact-inside-option'>Chat with us</button>
-          </div>
-        </div>
->>>>>>> 6e4210b281d077488b435a5f5f352b0a9a90fd11
         </div>
     );
 }
 
-<<<<<<< HEAD
 export default GetInTouch;
 
-=======
-export default GetInTouch;
->>>>>>> 6e4210b281d077488b435a5f5f352b0a9a90fd11
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 import { useRef } from 'react';
 import GetInTouch from '../components/contact/GetInTouch';
 import ContactForm from '../components/contact/ContactForm';
 import PartnerBar from '../components/contact/Partner-Bar';
+import '../components/contact/contact.css';
 
 function Contact() {
   const contactFormRef = useRef(null);
@@ -18,29 +18,6 @@ function Contact() {
       </div>
     </div>
   );
-=======
-import React from 'react'
-import GetInTouch from '../components/contact/GetInTouch';
-import ContactForm from '../components/contact/ContactForm';
-import { motion } from 'framer-motion';
-import '../components/contact/contact.css';
-
-
-function Contact({pageTransition}) {
-  return (
-    <motion.div
-    initial='initial'
-    animate='animate'
-    exit='exit'
-    variants={pageTransition}
-  >
-    <div className='container'>
-     <GetInTouch />
-     <ContactForm />
-    </div>
-    </motion.div>
-  )
->>>>>>> 6e4210b281d077488b435a5f5f352b0a9a90fd11
 }
 
 export default Contact;
